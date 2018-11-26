456bb8dcf9d94c01a55e9bc27a3548a690c14c79
HyunKook Park <gusrnr5153@cu.ac.kr>
2018-11-26 10:42:17 +0900

    Unit21.py

---
+++ Unit21.py
@@ -0,0 +1,20 @@
+import numpy as np
+numbers=np.array(range(1,11),copy=True)
+numbers
+
+ones=np.ones([2,4],dtype=np.float64)
+ones
+
+zeros=np.zeros([2,4],dtype=np.float64)
+zeros
+
+empty=np.empty([2,4],dtype=np.float64)
+empty
+
+eye=np.eye(3,k=1)
+eye
+
+np_numbers=np.arange(2,5,0.25)
+np_numbers
+
+ones.ndim
