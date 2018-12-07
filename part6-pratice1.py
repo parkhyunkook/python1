916dcb9c28719eeff7e157b9dafd38c111ea2127
HyunKook Park <45028740+parkhyunkook@users.noreply.github.com>
2018-12-07 13:22:20 +0900

    Update part6-pratice1.py

--- part6-pratice1.py
+++ part6-pratice1.py
@@ -3,5 +3,5 @@ a=pd.read_csv("lynx.csv")
 a["Year"]=lynx.Year/10
 a["Year"]=a["Year"].round()*10
 sum_a=a.groupby("Year").sum()
-result=sum_lynx.sort_values("huneted",ascending= False)
+result=sum_a.sort_values("huneted",ascending= False)
 print(result)
