8c0920261d896de6af4d8d61ff22319a9fad32c2
HyunKook Park <gusrnr5153@cu.ac.kr>
2018-12-13 15:09:15 +0900

    Unit51.py

---
+++ Unit51.py
@@ -0,0 +1,25 @@
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd, numpy.random as rnd
+import matplotlib, matplotlib.pyplot as plt
+
+hed= pd.read_csv("Hedonic.csv")
+selection=rnd.binomial(1,0.7,size=len(hed)).astype(bool)
+training=hed[selection]
+testing=hed[~selection]
+
+rfr=RandomForestRegressor()
+predictors_tra=training.ix[:,"crim":"lstat"]
+predictors_tst=testing.ix[:,"crim":"lstat"]
+
+feature="medv"
+rfr.fit(predictors_tra,training[feature])
+
+matplotlib.style
+plt.scatter(training[feature],rfr.predict(predictors_tra),c="green",s=50)
+
+plt.scatter(testing[feature],rfr.predict(predictors_tst),c="red")
+plt.legend(["Traning data","Testing data"],loc="upper left")
+plt.title("Hedonic Prices of Census Tracts in the Boston Area")
+plt.xlabel("Actual value")
+plt.ylabel("Predicted value")
+plt.savefig("rfr.pdf")
